325dda8e9eed88c6ab8338fad5ec50d4a0978ddc
DESKTOP-JR4PBRF\JIN <hanj8980@gmail.com>
2025-04-04 13:13:36 +0900

    # tab component 개발 - button component 개발 - [ ] button style 추가

--- fe-engineering-establishment-governance/app/FAQ/page.tsx
+++ fe-engineering-establishment-governance/app/FAQ/page.tsx
@@ -1,5 +1,5 @@
 import PageDescription from '@/app/component/PageDescription';
-
+import Tab from '@/app/component/tab/Tab';
 import styles from '@/app/FAQ/page.module.css';
 
 export default function FAQ() {
@@ -7,7 +7,14 @@ export default function FAQ() {
     <div className={styles.content}>
       <main className={styles.main}>
         <PageDescription />
-        <div></div>
+        <div>
+          <Tab>
+            {/* <input>
+            </input> */}
+            {/* <Filter /> */}
+            {/* <List /> */}
+          </Tab>
+        </div>
       </main>
       <footer className={styles.footer}></footer>
     </div>
