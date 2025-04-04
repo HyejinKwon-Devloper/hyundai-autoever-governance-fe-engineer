0e9343d66af9f454cb44784aac7cda1452ef0e83
DESKTOP-JR4PBRF\JIN <hanj8980@gmail.com>
2025-04-04 09:05:49 +0900

    - navlist component화

--- fe-engineering-establishment-governance/app/component/appbar/AppBar.tsx
+++ fe-engineering-establishment-governance/app/component/appbar/AppBar.tsx
@@ -1,8 +1,11 @@
 'use client';
 import Image from 'next/image';
 import Link from 'next/link';
-import styles from '@/app/appbar.module.css';
+import NavList from '@/app/component/appbar/NavList';
+
 import { useState } from 'react';
+
+import styles from '@/app/component/appbar/appbar.module.css';
 
 export default function Header() {
   const [isNavOpen, setNavOpen] = useState<boolean>();
@@ -25,20 +28,7 @@ export default function Header() {
             </Link>
           </div>
           <div className={styles.navlist}>
-            <ul>
-              <li>
-                <Link href={'/Guide'}>서비스 소개</Link>
-              </li>
-              <li>
-                <Link href={'/FAQ'}>자주 묻는 질문</Link>
-              </li>
-              <li>
-                <Link href={'/News'}>새소식</Link>
-              </li>
-              <li>
-                <Link href={'/Counsel'}>상담문의</Link>
-              </li>
-            </ul>
+            <NavList />
           </div>
           <div className={styles.nav}>
             <button onClick={handleNavMenu}>
@@ -49,22 +39,7 @@ export default function Header() {
       </header>
       {isNavOpen && (
         <div className={styles.menu}>
-          <nav>
-            <ul>
-              <li>
-                <Link href={'/Guide'}>서비스 소개</Link>
-              </li>
-              <li>
-                <Link href={'/FAQ'}>자주 묻는 질문</Link>
-              </li>
-              <li>
-                <Link href={'/News'}>새소식</Link>
-              </li>
-              <li>
-                <Link href={'/Counsel'}>상담문의</Link>
-              </li>
-            </ul>
-          </nav>
+          <NavList />
         </div>
       )}
     </>
--- fe-engineering-establishment-governance/app/layout.tsx
+++ fe-engineering-establishment-governance/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import Header from '@/app/AppBar';
+import Header from '@/app/component/appbar/AppBar';
 
 import '@/app/globals.css';
 
