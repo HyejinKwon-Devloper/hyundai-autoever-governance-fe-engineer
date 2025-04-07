4771d4d0a448fe03de7dcca80e8e216d7a754076
DESKTOP-JR4PBRF\JIN <hanj8980@gmail.com>
2025-04-07 10:47:17 +0900

    - 오타 제거

--- fe-engineering-establishment-governance/app/layout.tsx
+++ fe-engineering-establishment-governance/app/layout.tsx
@@ -29,7 +29,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <Header />`<MSWBootstrap>` {children}</MSWBootstrap>
+        <Header />
+        <MSWBootstrap> {children}</MSWBootstrap>
       </body>
     </html>
   );
