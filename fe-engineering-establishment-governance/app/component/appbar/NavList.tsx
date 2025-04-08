d09aa0e9e57d7a3c73c3c1be97f94304f5958f4e
DESKTOP-JR4PBRF\JIN <hanj8980@gmail.com>
2025-04-09 01:28:10 +0900

    feature(4) : AppBar 컴포넌트 수정 - Nav 메뉴에서 메뉴 클릭시 닫히게 변경 - 구조 변경 - hamburger bar transition 수정

--- fe-engineering-establishment-governance/app/component/appbar/NavList.tsx
+++ fe-engineering-establishment-governance/app/component/appbar/NavList.tsx
@@ -1,19 +1,28 @@
 import Link from 'next/link';
-export default function NavList() {
+export default function NavList(props: { handleNavMenu: () => void }) {
+  const { handleNavMenu } = props;
   return (
     <nav>
       <ul>
         <li>
-          <Link href={'/Guide'}>서비스 소개</Link>
+          <Link href={'/Guide'} onClick={handleNavMenu}>
+            서비스 소개
+          </Link>
         </li>
         <li>
-          <Link href={'/FAQ'}>자주 묻는 질문</Link>
+          <Link href={'/FAQ'} onClick={handleNavMenu}>
+            자주 묻는 질문
+          </Link>
         </li>
         <li>
-          <Link href={'/News'}>새소식</Link>
+          <Link href={'/News'} onClick={handleNavMenu}>
+            새소식
+          </Link>
         </li>
         <li>
-          <Link href={'/Counsel'}>상담문의</Link>
+          <Link href={'/Counsel'} onClick={handleNavMenu}>
+            상담문의
+          </Link>
         </li>
       </ul>
     </nav>
